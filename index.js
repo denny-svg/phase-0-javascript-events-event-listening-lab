0439c521fc7300cd3ec9536c69cbb033f8fa5d6d
denny_svg <sarah.wanjiku@student.moringaschool.com>
2023-12-29 17:03:47 +0300

    Done assignment

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
-function addingEventListener() {
-}
+// Code Here!
+ document.addEventListener('DOMContentLoaded', function () {
+    const input = document.getElementById('input');
+  
+    
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+  
+    input.addEventListener('click', clickAlert);
+  
+    
+    input.addEventListener('click', function() {
+      console.log('input listener clicked!');
+    });
+
+    addingEventListener();
+  });
+  
+  function addingEventListener() {
+    let input = document.getElementById('input');
+  
+    if (input) {
+      input.addEventListener('click', function() {
+        console.log('input listener clicked!');
+      });
+      return true;
+    } else {
+      console.log('input listener not found');
+      return false;
+    }
+  };
+
